ee35e79e8515303b36a86a2dd503bd67307dd9da
Sharif <fathifarhan21@hotmail.com>
2022-11-26 18:43:20 +0000

    --

---
+++ src/sum.test.ts
@@ -0,0 +1,7 @@
+const sum: any = require("./sum");
+
+const names: any = require("./sum");
+
+test("check names", () => {
+  expect(names("ayaan")).toBe("ayaan");
+});
---
+++ src/sum.ts
@@ -0,0 +1,9 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+module.exports = sum;
+
+function names(name: string) {
+  return name;
+}
+module.exports = names;
