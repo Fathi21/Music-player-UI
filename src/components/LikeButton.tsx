14d2b16fabadc907265a4a6c9f94f98f67146e43
Sharif <fathifarhan21@hotmail.com>
2022-12-12 16:47:45 +0000

    --

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LikeButton(props: any) {
   console.log(props);
-  return <i className="fas fa-heart"></i>;
+
+  const [redColor, setredColor] = useState("");
+
+  function handleClick() {
+    if (redColor) {
+      setredColor("");
+    } else {
+      setredColor("redColor");
+    }
+  }
+
+  return <i onClick={handleClick} className="fas fa-heart" id={redColor}></i>;
 }
 
 export default LikeButton;
