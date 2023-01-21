8c7aa61c16bbc6c4b863444733dcb2209057430c
Sharif <sharif@sharifs.home>
2023-01-21 21:34:47 +0000

    -

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { RoutePath } from "../Utilities/UrlPath/RoutePath";
-import { Link } from "react-router-dom";
 import GetAllSongs from "../Utilities/ApiCalls/GetAllSongs";
+import { Link, useLocation } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
 
 function Search() {
   const music = GetAllSongs();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const [searchInput, setSearchInput] = useState("");
 
@@ -20,46 +26,29 @@ function Search() {
 
   return (
     <div>
-      <i
-        className="fa fa-search"
-        aria-hidden="true"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-        data-bs-whatever="@mdo"
-      ></i>
-      Search
-      <div
-        className="modal fade"
-        id="exampleModal"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <div className="input-group searchInput">
-                <span className="input-group-text" id="basic-addon1">
-                  <i className="fa fa-search"></i>
-                </span>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search for a song..."
-                  aria-label="Search"
-                  aria-describedby="basic-addon1"
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  value={searchInput}
-                />
-              </div>
-            </div>
-            <div className="modal-body">
-              <div className="DocSearch-Hit-source">Results</div>
-              <div className="list-group searchedData">{listMusic}</div>
-            </div>
-            <div className="modal-footer"></div>
+      <i className="fa fa-search" onClick={handleShow}></i> Search
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <div className="input-group searchInput">
+            <span className="input-group-text" id="basic-addon1">
+              <i className="fa fa-search"></i>
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search for a song..."
+              aria-label="Search"
+              aria-describedby="basic-addon1"
+              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchInput}
+            />
           </div>
-        </div>
-      </div>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="list-group searchedData">{listMusic}</div>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </div>
   );
 }
